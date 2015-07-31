70a199405fcc07fabed033b98a05c518abf7d9c2
Cody Bouscaren <codybousc@gmail.com>
2015-07-31 10:37:59 -0700

    attempt to fix heroku database issue

--- db/migrate/20150730180416_drop_admins_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAdminsTable < ActiveRecord::Migration
-  def change
-    drop_table :admins
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20150730180824_drop_admins_table_three.rb
+++
@@ -1,9 +0,0 @@
-class DropAdminsTableThree < ActiveRecord::Migration
-  def up
-    drop_table :admins
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
